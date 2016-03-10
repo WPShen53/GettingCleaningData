a73e1a26d8e6c7a9b13381a53c44510f5ce6572f
WPShen53 <WPShen53@users.noreply.github.com>
2016-03-11 07:56:47 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@ features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 ## Load training set
 train_data <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "", 
                          na.strings ="", stringsAsFactors= FALSE)
+colnames(train_data) <- features
 
 train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 train_data$subject <- as.vector(train_subject[,1])
@@ -18,6 +19,7 @@ train_data$activity <- as.factor(train_labels[,1])
 ## Load testing set 
 test_data <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "" , header = F , 
                         na.strings ="", stringsAsFactors= F)
+colnames(test_data) <- features
 
 test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 test_data$subject <- as.vector(test_subject[,1])
@@ -27,20 +29,18 @@ test_data$activity <- as.factor(test_labels[,1])
 
 ## 1. Merges the training and the test sets to create one data set.
 total <- rbind(train_data, test_data)
-colnames(total) <- features
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-col_vector <- grep("subject|activity|mean|Mean|std", names(total))
+col_vector <- grep("subject|activity|mean|std", names(total))
 total <- total[,col_vector]
 
 ## 3. Uses descriptive activity names to name the activities in the data set
 levels(total$activity) <- activities
 
-## 4. Appropriately labels the data set with descriptive variable names. (was done in Step 1)
+## 4. Appropriately labels the data set with descriptive variable names. (was done in data loading)
 
 ## 5. creates a tidy data set with the average of each variable for each activity and each subject.
-avg <- total %>% group_by(subject, label) %>% summarise_each(funs(mean))
+avg <- total %>% group_by(subject, activity) %>% summarise_each(funs(mean))
 
 ### write to a text file
-write.table(avg, file = "w4_tidy_avg_dataset.txt", sep = ",", row.name = FALSE)
-
+write.table(avg, file = "w4_tidy_avg_dataset.csv", sep = ",", row.name = FALSE)
