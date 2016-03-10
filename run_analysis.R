4cf7788ce50a4e7b557e6175985837c17d895b53
WPShen53 <WPShen53@users.noreply.github.com>
2016-03-10 22:05:59 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,49 +2,43 @@
 library(dplyr)
 
 ## Load activity labels & features
-labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
 ## Load training set
 train_data <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "", 
                          na.strings ="", stringsAsFactors= FALSE)
 
-colnames(train_data) <- features
-
 train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 train_data$subject <- as.vector(train_subject[,1])
 
 train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
-train_labels <- as.factor(train_labels[,1])
-levels(train_labels) <- labels
-train_data$label <- train_labels
-
-col_vector <- grep("subject|label|mean|Mean|std", names(train_data))
-train_data <- train_data[,col_vector]
-
+train_data$activity <- as.factor(train_labels[,1])
 
 ## Load testing set 
 test_data <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "" , header = F , 
                         na.strings ="", stringsAsFactors= F)
 
-colnames(test_data) <- features
-
 test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 test_data$subject <- as.vector(test_subject[,1])
 
 test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
-test_labels <- as.factor(test_labels[,1])
-levels(test_labels) <- labels
-test_data$label <- test_labels
+test_data$activity <- as.factor(test_labels[,1])
 
-col_vector <- grep("subject|label|mean|Mean|std", names(test_data))
-test_data <- test_data[,col_vector]
-
-
-## merge testing and training dataset
+## 1. Merges the training and the test sets to create one data set.
 total <- rbind(train_data, test_data)
+colnames(total) <- features
 
-## create a tidy average dataset
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+col_vector <- grep("subject|activity|mean|Mean|std", names(total))
+total <- total[,col_vector]
+
+## 3. Uses descriptive activity names to name the activities in the data set
+levels(total$activity) <- activities
+
+## 4. Appropriately labels the data set with descriptive variable names. (was done in Step 1)
+
+## 5. creates a tidy data set with the average of each variable for each activity and each subject.
 avg <- total %>% group_by(subject, label) %>% summarise_each(funs(mean))
 
 ### write to a text file
